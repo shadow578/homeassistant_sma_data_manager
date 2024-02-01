f4aff8ea443ada35255f8213000f6f8d9d71f6af
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-02-01 13:19:15 +0000

    add tests for get_live_measurements and get_all_live_measurements

--- custom_components/sma_data_manager/sma/test/test_client.py
+++ custom_components/sma_data_manager/sma/test/test_client.py
@@ -4,6 +4,7 @@ import pytest
 from urllib.parse import quote
 
 from ..client import LOGIN_RESULT_ALREADY_LOGGED_IN, LOGIN_RESULT_NEW_TOKEN, LOGIN_RESULT_TOKEN_REFRESHED, SMAApiClient
+from ..model import LiveMeasurementQueryItem
 
 from .http_response_mock import ClientResponseMock
 
@@ -304,4 +305,220 @@ async def test_client_get_all_components():
 
 
 
+@pytest.mark.asyncio
+async def test_client_get_all_live_measurements():
+    """test SMAApiClient.get_all_live_measurements"""
+
+    # mock for make_request
+    did_get_measurements = False
+    async def make_request_mock(method: str, endpoint: str, data: dict|None = None, headers: dict|None = None, as_json: bool = True):
+        """mock for make_request"""
+        nonlocal did_get_measurements
+
+        # required for login
+        if method == "POST" and endpoint == "token":
+            return ClientResponseMock(
+                data={
+                    "access_token": "acc-token-1",
+                    "refresh_token": "ref-token-1",
+                    "token_type": "Bearer",
+                    "expires_in": 30, # ultra short-lived to test token refresh
+                },
+                cookies=[
+                    ("JSESSIONID", "session-id"),
+                ]
+            )
+
+
+        # POST /api/v1/measurements/live
+        if method == "POST" and endpoint == "measurements/live":
+            # check common headers
+            # origin headers
+            assert headers["Origin"] == "http://sma.local/api/v1"
+            assert headers["Host"] == "sma.local"
+            # session cookie
+            assert headers["Cookie"] == "JSESSIONID=session-id"
+            # auth header
+            assert headers["Authorization"] == "Bearer acc-token-1"
+            # content type headers
+            assert headers["Content-Type"] == "application/json"
+            assert headers["Accept"] == "application/json"
+
+            # body is json
+            assert as_json is True
+
+            # check payload
+            assert data is not None
+            assert data == [
+                {
+                    "componentId": "inv0",
+                },
+                {
+                    "componentId": "inv1",
+                },
+            ]
+
+            # return mock response
+            did_get_measurements = True
+            return ClientResponseMock(
+                data=[
+                    {
+                        "channelId": "chastt",
+                        "componentId": "inv0",
+                        "values": [
+                            {
+                                "time": "2024-02-01T11:30:00Z",
+                                "value": 10
+                            }
+                        ]
+                    },
+                    {
+                        "channelId": "chastt",
+                        "componentId": "inv1",
+                        "values": [
+                            {
+                                "time": "2024-02-01T11:30:00Z",
+                                "value": 25
+                            }
+                        ]
+                    },
+                ]
+            )
+
+        raise Exception(f"unexpected endpoint: {endpoint}")
+
+    # create the client
+    sma = SMAApiClient(
+        host="sma.local",
+        username="test",
+        password="test123",
+        session=mock.MagicMock(),
+        use_ssl=False,
+    )
+
+    # patch make_request
+    with mock.patch.object(sma, "make_request", wraps=make_request_mock):
+        assert (await sma.login()) == LOGIN_RESULT_NEW_TOKEN
+
+        # get all live measurements
+        measurements = await sma.get_all_live_measurements(
+            component_ids=["inv0", "inv1"]
+        )
+        assert did_get_measurements is True
+
+        assert len(measurements) == 2
+
+        # inv0
+        assert measurements[0].component_id == "inv0"
+        assert measurements[0].channel_id == "chastt"
+        assert measurements[0].values[0].time == "2024-02-01T11:30:00Z"
+        assert measurements[0].values[0].value == 10
+
+        # inv1
+        assert measurements[1].component_id == "inv1"
+        assert measurements[1].channel_id == "chastt"
+        assert measurements[1].values[0].time == "2024-02-01T11:30:00Z"
+        assert measurements[1].values[0].value == 25
+
+
+
+@pytest.mark.asyncio
+async def test_client_get_live_measurements():
+    """test SMAApiClient.get_live_measurements"""
+
+    # mock for make_request
+    did_get_measurement = False
+    async def make_request_mock(method: str, endpoint: str, data: dict|None = None, headers: dict|None = None, as_json: bool = True):
+        """mock for make_request"""
+        nonlocal did_get_measurement
+
+        # required for login
+        if method == "POST" and endpoint == "token":
+            return ClientResponseMock(
+                data={
+                    "access_token": "acc-token-1",
+                    "refresh_token": "ref-token-1",
+                    "token_type": "Bearer",
+                    "expires_in": 30, # ultra short-lived to test token refresh
+                },
+                cookies=[
+                    ("JSESSIONID", "session-id"),
+                ]
+            )
+
+
+        # POST /api/v1/measurements/live
+        if method == "POST" and endpoint == "measurements/live":
+            # check common headers
+            # origin headers
+            assert headers["Origin"] == "http://sma.local/api/v1"
+            assert headers["Host"] == "sma.local"
+            # session cookie
+            assert headers["Cookie"] == "JSESSIONID=session-id"
+            # auth header
+            assert headers["Authorization"] == "Bearer acc-token-1"
+            # content type headers
+            assert headers["Content-Type"] == "application/json"
+            assert headers["Accept"] == "application/json"
+
+            # body is json
+            assert as_json is True
+
+            # check payload
+            assert data is not None
+            assert data == [
+                {
+                    "componentId": "inv0",
+                    "channelId": "chastt",
+                },
+            ]
+
+            # return mock response
+            did_get_measurement = True
+            return ClientResponseMock(
+                data=[
+                    {
+                        "channelId": "chastt",
+                        "componentId": "inv0",
+                        "values": [
+                            {
+                                "time": "2024-02-01T11:30:00Z",
+                                "value": 10
+                            }
+                        ]
+                    },
+                ]
+            )
+
+        raise Exception(f"unexpected endpoint: {endpoint}")
+
+    # create the client
+    sma = SMAApiClient(
+        host="sma.local",
+        username="test",
+        password="test123",
+        session=mock.MagicMock(),
+        use_ssl=False,
+    )
+
+    # patch make_request
+    with mock.patch.object(sma, "make_request", wraps=make_request_mock):
+        assert (await sma.login()) == LOGIN_RESULT_NEW_TOKEN
+
+        # get live measurement
+        measurements = await sma.get_live_measurements([
+            LiveMeasurementQueryItem(
+                component_id="inv0",
+                channel_id="chastt",
+            )
+        ])
+        assert did_get_measurement is True
+
+        assert len(measurements) == 1
+
+        # inv0
+        assert measurements[0].component_id == "inv0"
+        assert measurements[0].channel_id == "chastt"
+        assert measurements[0].values[0].time == "2024-02-01T11:30:00Z"
+        assert measurements[0].values[0].value == 10
 
